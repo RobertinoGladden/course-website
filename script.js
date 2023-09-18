d1ffaa8af38a26f866df26cd5c26538118cc4a8e
Robertino <gladdenrobertino@gmail.com>
2023-09-18 11:00:37 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMore() {
+    var text = document.getElementById("text");
+    text.style.display = "block";
+    var button = document.getElementById("show-more-button");
+    button.style.display = "none";
+}
